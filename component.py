2355f27a65323b20c91049c699258c34a03c4db0
pupubird <rainchai42402gmail.com>
2019-05-03 09:01:05 +0800

    Initial commit

---
+++ component.py
@@ -0,0 +1,86 @@
+import curses
+import time
+import threading
+
+
+class Rectangle:
+
+    def __init__(self, window, name):
+        self.window = window
+        self.name = name
+
+    def draw_rectangle(self, up_left_y, up_left_x, low_right_y, low_right_x):
+        vertical_line = curses.ACS_VLINE
+        horizontal_line = "-"
+        corner_symb = "+"
+        """
+        Draw a rectangle with corners at the provided upper-left
+        and lower-right coordinates.
+        """
+        try:
+            # vertical line
+            self.window.vline(up_left_y+1, up_left_x, vertical_line,
+                              low_right_y - up_left_y - 1)
+            self.window.vline(up_left_y+1, low_right_x, vertical_line,
+                              low_right_y - up_left_y - 1)
+            # horizontal line
+            self.window.hline(up_left_y, up_left_x+1, horizontal_line,
+                              low_right_x - up_left_x - 1)
+            self.window.hline(low_right_y, up_left_x + 1, horizontal_line,
+                              low_right_x - up_left_x - 1)
+
+            # corner
+            self.window.addch(up_left_y, up_left_x, corner_symb)
+            self.window.addch(up_left_y, low_right_x, corner_symb)
+            self.window.addch(low_right_y, low_right_x, corner_symb)
+            self.window.addch(low_right_y, up_left_x, corner_symb)
+        except Exception:
+            return False
+        return True
+
+    @property
+    def content(self):
+        return self.content
+
+    @content.setter
+    def content(self, new_content):
+        self.content = new_content
+
+    @content.deleter
+    def content(self, del_content):
+        self.content = " "
+
+
+class LoadingAnimation:
+    def __init__(self, window):
+        self.window = window
+
+    def draw_loading(self, y, x):
+        animation_frame = ['-', '\\', '|', '/']
+        current_frame = 0
+        while True:
+            self.window.addstr(y, x, animation_frame[current_frame])
+            if current_frame == len(animation_frame) - 1:
+                current_frame = 0
+            else:
+                current_frame += 1
+            self.window.refresh()
+            time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    def main(window):
+        threadRunning = threading.Thread(target=another_function)
+        animation = threading.Thread(target=test, args=[window], daemon=True)
+
+        animation.start()
+        threadRunning.start()
+
+    def test(window):
+        test_ani = LoadingAnimation(window)
+        test_ani.draw_loading(0, 0)
+
+    def another_function():
+        time.sleep(3)
+
+    curses.wrapper(main)
